c3fdd49497c051fa1f8c1e57b0502377be800c77
ross-brown <rbrown_98@yahoo.com>
2023-01-31 17:00:51 -0600

    add solutions to all problems

---
+++ problems/16-suffix-cipher.js
@@ -0,0 +1,55 @@
+/*******************************************************************************
+Write a function `suffixCipher` that accepts a sentence and object as arguments.
+The object contains suffixes as keys and callbacks as values. The `suffixCipher`
+function should return a new sentence where words of the original sentence are
+modified according to the callback that corresponds with the suffix that the word
+ends with. If the word does not end in any of the suffix keys, then it should not
+be modified. You can assume that only one suffix of the object will match a word.
+
+Examples:
+
+let cipher1 = {
+    ly: function(word) {
+        return word.slice(0, -1) + 'ee';
+    },
+    ize: function(word) {
+        return word + 'r';
+    }
+};
+console.log(suffixCipher('quietly and gently visualize', cipher1));
+// quietlee and gentlee visualizer
+
+let cipher2 = {
+    tal: function(word) {
+        return word.toUpperCase();
+    },
+    s: function(word) {
+        return word + 'th';
+    }
+};
+console.log(suffixCipher('incremental progress is very instrumental', cipher2));
+// INCREMENTAL progressth isth very INSTRUMENTAL
+*******************************************************************************/
+
+let suffixCipher = function(sentence, object) {
+    //split sentence into words array
+    let words = sentence.split(' ');
+    //loop over words array
+    for (let i = 0; i < words.length; i++) {
+        let word = words[i];
+        //loop over object
+        for (let suffix in object) {
+            let callback = object[suffix];
+            //if the current word ends with the current KEY
+            if (word.endsWith(suffix)) {
+                //pass that word into the VALUE callback and reassign the word
+                words[i] = callback(word);
+            }
+        }
+    }
+    //return the words joined together
+    return words.join(' ');
+};
+
+/*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
+module.exports = suffixCipher;
